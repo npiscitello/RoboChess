7fee006f90a94d1efd49e21f83421b73958428c2
Nick Piscitello <npiscitello@users.noreply.github.com>
2018-02-26 18:09:27 -0500

    started developing serial functions

--- include/serial.h
+++ include/serial.h
@@ -1,4 +1,19 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
+/* read one byte from the UART receive FIFO
+ *  data: address at which to place the read data
+ *
+ *  returns: a code indicating the success of the operation
+ *      1   - operation completed successfully, queue contents remain
+ *      0   - operation completed successfully, queue empty
+ *      -1  - operation unsuccessful; queue is already empty
+ */
+int serial_read( unsigned char* data );
+
+/* write one byte to the UART send FIFO
+ *  data: data to push onto the queue
+ */
+void serial_write( unsigned char data );
+
 #endif
